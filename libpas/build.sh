65d04e856d287877ef0343ea30fca0e9e5461476
Filip Pizlo <pizlo@behemoth.local>
2023-12-20 10:36:46 -0800

    landing libpas

---
+++ libpas/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+do_build() {
+    libname=$1
+    flags=$2
+    mkdir -p build
+    rm -f build/*.o
+    for x in src/libpas/*.c
+    do
+        xcrun clang -O3 -W -Werror -c -o build/`basename $x .c`.o $x $flags &
+    done
+    wait
+    xcrun clang -dynamiclib -o build/$libname build/*.o
+}
+
+do_build libpas_test.dylib ""
+
