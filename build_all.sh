182574d40c468e555bd1776aebbe9715e9a198c6
Filip Pizlo <pizlo@behemoth.local>
2024-01-05 16:58:50 -0800

    more shit

--- build_all.sh
+++ build_all.sh
@@ -10,3 +10,5 @@ mkdir -p runtime-build
 (cd runtime-build && cmake -S ../../llvm-project-clean/llvm -B . -G Ninja -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_RUNTIMES=compiler-rt && ninja)
 
 (cd libpas && ./build.sh)
+
+(cd musl && CC=../build/bin/clang ./configure --target=aarch64 --prefix=prefix && make && make install-headers)
