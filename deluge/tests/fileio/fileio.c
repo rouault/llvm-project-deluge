722737a70f0051a58f75d3c9bcb27e203e5d981d
Filip Pizlo <pizlo@behemoth.local>
2024-01-17 11:20:45 -0800

    more shit

--- deluge/tests/fileio/fileio.c
+++ deluge/tests/fileio/fileio.c
@@ -1,5 +1,7 @@
 #include <stdfil.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char** argv)
 {
@@ -11,6 +13,15 @@ int main(int argc, char** argv)
     int result = zsys_read(fd, buf, sizeof(buf));
     ZASSERT(result == 14);
     zprintf("read the zsys way: %s", buf);
+    zsys_close(fd);
+
+    fd = open("deluge/tests/fileio/test.txt", O_RDONLY);
+    ZASSERT(fd > 2);
+    __builtin_memset(buf, 0, sizeof(buf));
+    result = read(fd, buf, sizeof(buf));
+    ZASSERT(result == 14);
+    zprintf("read with read way: %s", buf);
+    close(fd);
 
     FILE* fin = fopen("deluge/tests/fileio/test.txt", "r");
     ZASSERT(fin);
@@ -18,6 +29,7 @@ int main(int argc, char** argv)
     result = fread(buf, 1, sizeof(buf), fin);
     ZASSERT(result == 14);
     printf("read the fread way: %s", buf);
+    fclose(fin);
     
     return 0;
 }
