936ba10ad36b82cd303c4c4bf1f2a71c52127819
Filip Pizlo <pizlo@behemoth.local>
2024-02-15 18:00:00 -0800

    more shit

--- setup_gits.sh
+++ setup_gits.sh
@@ -5,7 +5,9 @@ set -x
 
 (git clone . llvm-project-clean && cd llvm-project-clean && git checkout 6009708b4367171ccdbf4b5905cb6a803753fe18)
 
-git clone ssh://www.filpizlo.com/home/pizlo/deluge-musl musl
+git clone https://github.com/pizlonator/deluded-musl musl
 
-git clone ssh://www.filpizlo.com/home/pizlo/zlib-1.3
-git clone ssh://www.filpizlo.com/home/pizlo/openssl-3.2.0
+git clone https://github.com/pizlonator/deluded-zlib-1.3.git zlib-1.3
+git clone https://github.com/pizlonator/deluded-openssl-3.2.0.git openssl-3.2.0
+git clone https://github.com/pizlonator/deluded-curl-8.5.0.git curl-8.5.0
+
