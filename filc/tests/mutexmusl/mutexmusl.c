7b59f7d34af58087030746141994e4b41c2c9578
Filip Pizlo <pizlo@behemoth.local>
2024-05-05 07:32:23 -0700

    Add trylock and recursive mutex tests. Corresponds to deluded-musl/4621ad67

--- filc/tests/mutexmusl/mutexmusl.c
+++ filc/tests/mutexmusl/mutexmusl.c
@@ -19,6 +19,26 @@ int main(int argc, char** argv)
     ZASSERT(!pthread_mutex_unlock(&mutex2));
     ZASSERT(!pthread_mutex_destroy(&mutex2));
 
+    pthread_mutexattr_t recursive_attr;
+    ZASSERT(!pthread_mutexattr_init(&recursive_attr));
+    ZASSERT(!pthread_mutexattr_settype(&recursive_attr, PTHREAD_MUTEX_RECURSIVE));
+    pthread_mutex_t recursive_mutex;
+    ZASSERT(!pthread_mutex_init(&recursive_mutex, &recursive_attr));
+    size_t index;
+    for (index = 100; index--;)
+        ZASSERT(!pthread_mutex_lock(&recursive_mutex));
+    for (index = 100; index--;)
+        ZASSERT(!pthread_mutex_trylock(&recursive_mutex));
+    for (index = 200; index--;)
+        ZASSERT(!pthread_mutex_unlock(&recursive_mutex));
+    for (index = 100; index--;)
+        ZASSERT(!pthread_mutex_trylock(&recursive_mutex));
+    for (index = 100; index--;)
+        ZASSERT(!pthread_mutex_lock(&recursive_mutex));
+    for (index = 200; index--;)
+        ZASSERT(!pthread_mutex_unlock(&recursive_mutex));
+    pthread_mutex_destroy(&recursive_mutex);
+
     printf("Bardzo dobrze\n");
     return 0;
 }
