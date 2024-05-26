9d48b1679c70cea04f20caa5e996d9c979b3c608
Phil Pizlo <pizlo@mac.com>
2024-05-26 19:37:21 +0000

    Fix i128 alignment on x86_64 (or rather, change the ABI to please my tastes).

--- clang/lib/Basic/Targets/X86.h
+++ clang/lib/Basic/Targets/X86.h
@@ -728,7 +728,7 @@ public:
                           : IsWinCOFF ? "e-m:w-p270:32:32-p271:32:32-p272:64:"
                                         "64-i64:64-f80:128-n8:16:32:64-S128"
                                       : "e-m:e-p270:32:32-p271:32:32-p272:64:"
-                                        "64-i64:64-f80:128-n8:16:32:64-S128");
+                                        "64-i64:64-i128:128-f80:128-n8:16:32:64-S128");
 
     // Use fpret only for long double.
     RealTypeUsesObjCFPRetMask = (unsigned)FloatModeKind::LongDouble;
