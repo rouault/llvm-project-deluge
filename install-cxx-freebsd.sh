35cc3c0f3afcf223b18b2876dafe8ec695d9994d
Filip Pizlo <pizlo@daath.pizlohouse.com>
2024-06-08 07:40:00 -0700

    Fix FreeBSD C++ library install so it works with more than one version.

--- install-cxx-freebsd.sh
+++ install-cxx-freebsd.sh
@@ -26,11 +26,13 @@
 set -e
 set -x
 
-cp build/lib/x86_64-unknown-freebsd14.0/libc++.so pizfix/lib
-cp build/lib/x86_64-unknown-freebsd14.0/libc++.so.1.0 pizfix/lib
-cp build/lib/x86_64-unknown-freebsd14.0/libc++abi.so.1.0 pizfix/lib
-cp build/lib/x86_64-unknown-freebsd14.0/libc++.a pizfix/lib
-cp build/lib/x86_64-unknown-freebsd14.0/libc++abi.a pizfix/lib
+VERSION=`uname -r | awk 'BEGIN { FS = "-" }; {print $1}'`
+
+cp build/lib/x86_64-unknown-freebsd$VERSION/libc++.so pizfix/lib
+cp build/lib/x86_64-unknown-freebsd$VERSION/libc++.so.1.0 pizfix/lib
+cp build/lib/x86_64-unknown-freebsd$VERSION/libc++abi.so.1.0 pizfix/lib
+cp build/lib/x86_64-unknown-freebsd$VERSION/libc++.a pizfix/lib
+cp build/lib/x86_64-unknown-freebsd$VERSION/libc++abi.a pizfix/lib
 (cd pizfix/lib &&
      rm -f libc++.so.1 &&
      rm -f libc++abi.so.1 &&
