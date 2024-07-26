4c8ff5f4dbc0e0600031105ffcfc5c788f890f1e
Phil Pizlo <phil.pizlo@epicgames.com>
2024-07-25 17:10:50 -0700

    Fix package-build to work with the new Linux-based approach.

--- package-build.sh
+++ package-build.sh
@@ -28,7 +28,7 @@
 set -e
 set -x
 
-build_name=filc-0.666.1-$OS-$ARCH
+build_name=filc-0.667-$OS-$ARCH
 
 rm -rf $build_name
 
@@ -44,12 +44,6 @@ cp build/bin/clang-17 $build_name/build/bin/
 mkdir -p $build_name/build/include/
 cp -R build/include/c++ $build_name/build/include/
 
-if test $OS = macosx
-then
-    mkdir -p $build_name/runtime-build/lib/clang/17/lib/
-    cp -R runtime-build/lib/clang/17/lib/darwin $build_name/runtime-build/lib/clang/17/lib/
-fi
-
 cp -R pizfix $build_name/
 rm $build_name/pizfix/etc/moduli
 rm $build_name/pizfix/etc/ssh_host*
@@ -62,67 +56,33 @@ echo '#!/bin/sh' > setup.sh
 echo 'set -e' >> setup.sh
 echo 'set -x' >> setup.sh
 
-case $OS in
-    macosx)
-        fix_paths()
-        {
-            filename=$1
-            for old_path in `otool -L $filename | awk '{print $1}'`
-            do
-                if echo $old_path | grep -q $sourcedir
-                then
-                    new_path=pizfix/lib/`basename $old_path`
-                    install_name_tool -change $old_path $new_path $filename
-                    echo "install_name_tool -change $new_path \$PWD/$new_path $filename" >> setup.sh
-                fi
-            done
-        }
-        
-        for dylib in pizfix/lib/*.dylib
-        do
-            if test ! -L $dylib
-            then
-                old_name=`otool -L $dylib | head -2 | tail -1 | awk '{print $1}'`
-                new_name=pizfix/lib/`basename $old_name`
-                install_name_tool -id $new_name $dylib
-                echo "install_name_tool -id \$PWD/$new_name $dylib" >> setup.sh
-                fix_paths $dylib
-            fi
-        done
-        for binary in pizfix/bin/* pizfix/sbin/* pizfix/libexec/*
-        do
-            fix_paths $binary
-        done
-        ccprefix="xcrun "
-        ;;
-    freebsd)
-        for binary in pizfix/lib/*.so pizfix/lib/*.so.* pizfix/bin/* pizfix/sbin/* pizfix/libexec/*
-        do
-            if test ! -L $binary
-            then
-                if patchelf --set-rpath pizfix/lib $binary
-                then
-                    echo "patchelf --set-rpath \$PWD/pizfix/lib $binary" >> setup.sh
-                fi
-            fi
-        done
-        ccprefix=""
-        ;;
-    *)
-        echo "Bad OS"
-        exit 1
-        ;;
-esac
+for binary in pizfix/lib/*.so pizfix/lib/*.so.* pizfix/lib64/*.so pizfix/lib64/*.so.* pizfix/bin/* pizfix/sbin/* pizfix/libexec/*
+do
+    if test ! -L $binary
+    then
+        if patchelf --set-rpath pizfix/yolo/lib:pizfix/lib64:pizfix/lib $binary
+        then
+            echo "patchelf --set-rpath \$PWD/pizfix/yolo/lib:\$PWD/pizfix/lib64:\$PWD/pizfix/lib $binary" >> setup.sh
+        fi
+        if patchelf --set-interpreter pizfix/yolo/lib/ld-musl-x86_64.so.1 $binary
+        then
+            echo "patchelf --set-interpreter \$PWD/pizfix/yolo/lib/ld-musl-x86_64.so.1 $binary" >> setup.sh
+        fi
+    fi
+done
+
+rm pizfix/yolo/lib/ld-musl-x86_64.so.1
+(cd pizfix/yolo/lib/ && ln -s libyolomusl.so ld-musl-x86_64.so.1)
 
 echo 'set +x' >> setup.sh
 echo 'echo' >> setup.sh
 echo 'echo "You are all set. Try compiling something with:"' >> setup.sh
 echo 'echo' >> setup.sh
-echo "echo \"    ${ccprefix}build/bin/clang -o whatever whatever.c -O -g\"" >> setup.sh
+echo "echo \"    build/bin/clang -o whatever whatever.c -O -g\"" >> setup.sh
 echo 'echo' >> setup.sh
 echo 'echo "or:"' >> setup.sh
 echo 'echo' >> setup.sh
-echo "echo \"    ${ccprefix}build/bin/clang++ -o whatever whatever.cpp -O -g\"" >> setup.sh
+echo "echo \"    build/bin/clang++ -o whatever whatever.cpp -O -g\"" >> setup.sh
 echo 'echo' >> setup.sh
 echo 'echo "Or, try running something, like:"' >> setup.sh
 echo 'echo' >> setup.sh
